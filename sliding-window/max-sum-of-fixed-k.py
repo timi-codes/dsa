5ff40ba33dc8e0b6600e8ca2d386fd33593fefac
ttejumola <tejumoladavid@gmail.com>
2024-02-28 23:54:55 -0500

    sliding-window: max sum of fixed k

---
+++ sliding-window/max-sum-of-fixed-k.py
@@ -0,0 +1,26 @@
+import sys
+
+class Solution:
+    """
+    Find the max of the sum of subarray of fixed size k
+    [4, 2, 1, 7, 8, 1, 2, 8, 1, 0]
+    """
+    def findMaxSumOfSubArray(self, arr, k):
+        maxValue = ~sys.maxsize #smallest represenattion of int
+        currentSum = 0
+
+        for i in range(0, len(arr)):
+            currentSum += arr[i]
+
+            if i >= k:
+                currentSum -= arr[i-k]
+                maxValue = max(currentSum, maxValue)
+
+        return maxValue
+    
+solution = Solution()
+arr1 = [4, 2, 1, 7, 8, 1, 2, 8, 1, 0]
+arr2 = [5,4,-1,7,8]
+arr3 = [-2,1,-3,4,-1,2,1,-5,4]
+
+print(solution.findMaxSumOfSubArray(arr2, 3))
