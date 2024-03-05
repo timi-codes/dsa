0e6d780767ff4baf5b620e4afb00f7a2242252c6
--replace <tejumoladavid@gmail.com>
2024-03-05 13:57:38 -0500

    long subarray with k distinct

--- sliding-window/long-subarr-with-k-distinct-char.py
+++ sliding-window/long-subarr-with-k-distinct-char.py
@@ -20,7 +20,12 @@ class Solution:
             if len(dict) >= (k + 1):
                 charAtWindowStart = arr[windowStart]
                 dict[charAtWindowStart] -=1
-                distictCharCount = min(distictCharCount, (windowEnd - windowStart) + 1)
+                
+                if dict[charAtWindowStart] == 0:
+                    del dict[charAtWindowStart]
+
+                distictCharCount = min(distictCharCount, windowEnd - windowStart)
+
                 windowStart += 1
 
             print(dict)
@@ -31,7 +36,5 @@ class Solution:
     
 solution = Solution()
 arr1 = ["A", 'A', 'A', 'H', 'H', 'I', 'B', 'C']
-# arr2 = [5,4,-1,7,8]
-# arr3 = [-2,1,-3,4,-1,2,1,-5,4]
 
-print(solution.longestSubStrLength(arr1, 2))
+print(solution.longestSubStrLength(arr1, 1))
